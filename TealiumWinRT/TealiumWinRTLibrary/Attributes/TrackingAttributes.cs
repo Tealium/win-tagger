5c4bf59a1c1078915af24c63f4ee98ae8ec61116
S Rettinger <srettinger@blitzagency.com>
2013-01-29 14:28:50 -0600

    initial check-in of tealium winrt library

---
+++ TealiumWinRT/TealiumWinRTLibrary/Attributes/TrackingAttributes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tealium
+{
+    public class TrackPropertyAttribute : TrackBaseAttribute
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class TrackPageViewAttribute : TrackBaseAttribute
+    {
+        public TrackPageViewAttribute(string pageName)
+        {
+            this.Value = pageName;
+        }
+        public string Value { get; set; }
+    }
+
+    public class TrackBaseAttribute : Attribute
+    {
+
+    }
+
+}
